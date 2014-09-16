b20d6157071e7ab91e72fa857c22e417ca890920
Daniel Alves <daniel@danielalves.me>
2014-09-16 17:11:49 +0100

    Specs for new operators

--- spec/lib/query_engine/matchable_spec.rb
+++ spec/lib/query_engine/matchable_spec.rb
@@ -31,10 +31,10 @@ module QueryEngine
       expect(described_class.matches?(document, c: { d: 3})).to be_falsy
       expect(described_class.matches?(document, a: { '$all' => [1, 2]})).to be_truthy
       expect(described_class.matches?(document, a: { '$all' => [1, 2, 3]})).to be_falsy
-      expect(described_class.matches?(document, a: { '$or' => [1, 3]})).to be_truthy
-      expect(described_class.matches?(document, a: { '$or' => [3, 6]})).to be_falsy
-      expect(described_class.matches?({a: 4}, a: { '$or' => [3, 6]})).to be_falsy
-      expect(described_class.matches?({a: 3}, a: { '$or' => [3, 6]})).to be_truthy
+      expect(described_class.matches?(document, a: { '$any' => [1, 3]})).to be_truthy
+      expect(described_class.matches?(document, a: { '$any' => [3, 6]})).to be_falsy
+      expect(described_class.matches?({a: 4}, a: { '$any' => [3, 6]})).to be_falsy
+      expect(described_class.matches?({a: 3}, a: { '$any' => [3, 6]})).to be_truthy
       expect(described_class.matches?(document, c: { d: {'$gt' => 3}})).to be_truthy
       expect(described_class.matches?(document, c: { d: {'$gt' => 4}})).to be_falsy
       expect(described_class.matches?(document, c: { d: {'$lt' => 5}})).to be_truthy
@@ -45,19 +45,119 @@ module QueryEngine
       expect(described_class.matches?(document, c: { d: {'$gte' => 5}})).to be_falsy
     end
 
-    it 'should find in complex document' do
-      expect(described_class.matches?(complex_document, {'conditions' => ['clear']})).to be_truthy
-      expect(described_class.matches?(complex_document, {'conditions' => {'$or' => ['clear', 'half clear']}})).to be_truthy
-      expect(described_class.matches?(complex_document, {'conditions' => {'$or' => ['cloudy', 'half clear']}})).to be_falsy
+    # it 'should find in complex document' do
+    #   expect(described_class.matches?(complex_document, {'conditions' => ['clear']})).to be_truthy
+    #   expect(described_class.matches?(complex_document, {'conditions' => {'$or' => ['clear', 'half clear']}})).to be_truthy
+    #   expect(described_class.matches?(complex_document, {'conditions' => {'$or' => ['cloudy', 'half clear']}})).to be_falsy
+    # end
+
+    let(:raining_weekday) do
+      {
+        '#weather' => {
+          conditions: ['raining'],
+          temperature: {
+            max: 15,
+            min: 10,
+            average: 12.5
+          }
+        },
+        '#time' => {
+          weekday: 3,
+          hour: 15,
+          minute: 7
+        }
+      }
     end
 
-    it 'should change context when an outer operator receives a hash' do
-      expect(described_class.matches?(complex_document, {'$or' => {'conditions' => {'$or' => ['clear']}, 'temperature' => '40'}})).to be_truthy
-      expect(described_class.matches?(complex_document, {'$or' => {'conditions' => {'$or' => ['cloudy']}, 'temperature' => {'min' => 10}}})).to be_truthy
-      expect(described_class.matches?(complex_document, {'$or' => {'conditions' => {'$or' => ['cloudy']}, 'temperature' => {'min' => 15}}})).to be_falsy
-      expect(described_class.matches?(complex_document, {'temperature' => {'$or' => {'max' => 20, 'min' => -5}}})).to be_truthy
-      expect(described_class.matches?(complex_document, {'temperature' => {'$and' => {'max' => 20, 'min' => -5}}})).to be_falsy
-      expect(described_class.matches?(complex_document, {'temperature' => {'$and' => {'max' => 20, 'min' => 10}}})).to be_truthy
+    let(:one) do
+      {
+        '$and' => [
+          {
+            '#weather' => {
+              conditions: { '$any' => ['raining', 'light_rain'] },
+            }
+          },
+          {
+            '#time' => {
+              weekday: { '$any' => [1, 2, 3, 4, 5] }
+            }
+          }
+        ]
+      }
+    end
+
+    let(:two) do
+      {
+        '$and' => [
+          {
+            '#weather' => {
+              conditions: { '$any' => ['light_rain'] },
+            }
+          },
+          {
+            '#time' => {
+              weekday: { '$any' => [1, 2] }
+            }
+          }
+        ]
+      }
+    end
+
+    let(:three) do
+      {
+        '$or' => [
+          {
+            '#weather' => {
+              conditions: { '$any' => ['raining'] },
+            }
+          },
+          {
+            '#time' => {
+              weekday: { '$any' => [1, 2] }
+            }
+          }
+        ]
+      }
+    end
+
+    let(:four) do
+      {
+        '$and' => [
+          {
+            '#weather' => {
+              conditions: { '$any' => ['raining'] },
+            }
+          },
+          {
+            '$or' => [
+              {
+                '#time' => { weekday: 5 }
+              },
+              {
+                '#weather' => {
+                  temperature: { max: {'$lt' => 10} }
+                }
+              }
+            ]
+          }
+        ]
+      }
+    end
+
+    let(:five) do
+      {
+        '#weather' => {
+          temperature: { max: 15 }
+        }
+      }
+    end
+
+    it 'matches complex queries' do
+      expect(described_class.matches?(raining_weekday, one)).to be_truthy
+      expect(described_class.matches?(raining_weekday, two)).to be_falsey
+      expect(described_class.matches?(raining_weekday, three)).to be_truthy
+      expect(described_class.matches?(raining_weekday, four)).to be_falsey
+      expect(described_class.matches?(raining_weekday, five)).to be_truthy
     end
   end
 end
---
+++ spec/lib/query_engine/queries_spec.rb
@@ -0,0 +1,131 @@
+require 'spec_helper'
+
+module QueryEngine
+  describe Matchable do
+
+    it 'returns true when query is empty' do
+      expect(described_class.matches?({a: 1}, {})).to be_truthy
+      expect(described_class.matches?({b: 'a'}, {})).to be_truthy
+    end
+
+    describe '$all' do
+      let(:query) do
+        { a: { '$all' => ['a', 'b'] } }
+      end
+
+      let(:examples) do
+        [
+          [{ a: [ 'a', 'b' ] }, true],
+          [{ a: [ 'b', 'a' ] }, true],
+          [{ a: 'a' }, false],
+          [{ a: 'b' }, false],
+          [{ a: 'd' }, false],
+          [{ b: 'a'}, false],
+          [{}, false]
+        ]
+      end
+
+      it 'should matches correctly' do
+        examples.each do |example|
+          expect(described_class.matches?(example[0], query)).to eq(example[1])
+        end
+      end
+    end
+
+    describe '$any' do
+      let(:query) do
+        { a: { '$any' => [ 'a', 'b' ] } }
+      end
+
+      let(:examples) do
+        [
+          [{ a: [ 'a', 'b' ] }, true],
+          [{ a: [ 'b', 'a' ] }, true],
+          [{ a: [ 'a' ] }, true],
+          [{ a: [ 'b' ] }, true],
+          [{ a: 'a' }, true],
+          [{ a: 'b' }, true],
+          [{ a: [ 'a', 'b', 'c' ]}, true],
+          [{ a: 'd' }, false],
+          [{ b: 'a'}, false],
+          [{}, false]
+        ]
+      end
+
+      it 'should matches correctly' do
+        examples.each do |example|
+          expect(described_class.matches?(example[0], query)).to eq(example[1])
+        end
+      end
+    end
+
+    describe '$gt' do
+      let(:query) do
+        { a: { '$gt' => 1 } }
+      end
+
+      let(:examples) do
+        [
+          [{ a: 3 }, true],
+          [{ a: 2 }, true],
+          [{ a: 1 }, false],
+          [{ a: 0 }, false],
+          [{ b: 2 }, false],
+          [{}, false],
+          [{ a: 1, b: 1 }, false],
+          [{ a: 2, b: 1 }, true]
+        ]
+      end
+
+      it 'should matches correctly' do
+        examples.each do |example|
+          expect(described_class.matches?(example[0], query)).to eq(example[1])
+        end
+      end
+    end
+
+    describe '$gte' do
+      let(:query) do
+        { a: { '$gte' => 1 } }
+      end
+
+      let(:examples) do
+        [
+          [{ a: 3 }, true],
+          [{ a: 2 }, true],
+          [{ a: 1 }, true],
+          [{ a: 0 }, false],
+          [{ b: 2 }, false],
+          [{ a: 1, b: 1 }, true],
+          [{ a: 2, b: 1 }, true]
+        ]
+      end
+
+      it 'should matches correctly' do
+        examples.each do |example|
+          expect(described_class.matches?(example[0], query)).to eq(example[1])
+        end
+      end
+    end
+
+    describe '$in' do
+      let(:query) do
+        { foo: { "$in" => ["a", "b"] } }
+      end
+
+      let(:examples) do
+        [
+          [{ foo: "a" }, true],
+          [{ foo: "b" }, true],
+          [{ foo: "c" }, false]
+        ]
+      end
+
+      it 'should matches correctly' do
+        examples.each do |example|
+          expect(described_class.matches?(example[0], query)).to eq(example[1])
+        end
+      end
+    end
+  end
+end
